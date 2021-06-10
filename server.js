2c3cdd21fd058c8fce502d479eecc43ae707f4c5
Víctor Gallardo <vbubobuho@gmail.com>
2021-06-10 23:13:57 +0200

    Ajustes para prod

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(static(__dirname + '/dist/i-admin'));
+app.use(express.static(__dirname + '/dist/i-admin'));
 app.get('/*', function(req,res) {
-  res.sendFile(join(__dirname+
+  res.sendFile(path.join(__dirname+
     '/dist/i-admin/index.html'));});
 app.listen(process.env.PORT || 8080);
